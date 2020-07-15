ae621da2cd5f35ea5bf759d85d0dde5855b87fe8
Felix Chen <felixchen1998@gmail.com>
2020-07-15 13:11:36 -0400

    user signup + auth

--- src/backend/couchdb.js
+++ src/backend/couchdb.js
@@ -1,25 +1,67 @@
 const Cloudant = require("@cloudant/cloudant");
+const bcrypt = require("bcrypt");
 const serviceCredentials = require("./secrets.json").couchdb;
 
 const databaseInitCallback = function (err, cloudant, pong) {
-    if (err) {
-        return console.log("Failed to initialize Cloudant: " + err.message);
-    }
-    console.log(pong); // {"couchdb":"Welcome","version": ...
-    // Lists all the databases.
-    cloudant.db
-        .list()
-        .then((body) => {
-            body.forEach((db) => {
-                console.log(db);
-            });
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-}
+  if (err) {
+    return console.log("Failed to initialize Cloudant: " + err.message);
+  }
+  console.log(pong); // {"couchdb":"Welcome","version": ...
+  // Lists all the databases.
+  console.log("Databases: ");
+  cloudant.db
+    .list()
+    .then((body) => {
+      body.forEach((db) => {
+        console.log(db);
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
 const cloudant = Cloudant(serviceCredentials, databaseInitCallback);
+const db = cloudant.db.use("redsweater");
+
+const addUser = function (email, password) {
+  let documentName = "users";
+  db.get(documentName, function (err, data) {
+    // Retrieve Doc
+    if (err) {
+      return console.log("Failed: " + err.message);
+    }
+    doc = data;
+
+    //  Make change
+    doc["users"][email] = {
+      password: bcrypt.hashSync(password, 1),
+    };
+
+    // Write
+    db.insert(doc, function (err, data) {
+      if (err) {
+        return console.log("Failed: " + err.message);
+      }
+    });
+  });
+};
+
+const auth = async function (email, password) {
+  let documentName = "users";
+  let doc = await db.get(documentName);
+  hash = doc["users"][email]["password"];
+  return bcrypt.compareSync(password, hash);
+};
+
+
+const main = async function () {
+  user = "felixchen1998@gmail.com";
+
+  console.log(await auth(user, "aa"));
+  console.log(await auth(user, "aaa"));
+
+}()
 
 // https://github.com/cloudant/nodejs-cloudant
 // https://github.com/cloudant/haengematte/blob/master/nodejs/crud.js
