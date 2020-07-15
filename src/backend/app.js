cfbf71f83b622e4c187c3b8733cbce0a97602e99
Felix Chen <felixchen1998@gmail.com>
2020-07-14 23:40:28 -0400

    switched to express

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,19 +1,40 @@
-const io = require('socket.io')(3000);
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
 
-io.on('connect', socket => {
+// app.get('/', function (req, res) {
+//   res.sendFile(__dirname + '/index.html');
+// });
+
+// io.on('connection', function (socket) {
+//   socket.on('chat message', function (msg) {
+//     io.emit('chat message', msg);
+//   });
+// });
+
+
+io.on("connect", (socket) => {
   // either with send()
-  socket.send('Hello!');
+  socket.send("Hello!");
 
   // or with emit() and custom event names
-  socket.emit('greetings', 'Hey!', { 'ms': 'jane' }, Buffer.from([4, 3, 3, 1]));
+  socket.emit("greetings", "Hey!", {
+    ms: "jane"
+  }, Buffer.from([4, 3, 3, 1]));
 
   // handle the event sent with socket.send()
-  socket.on('message', (data) => {
+  socket.on("message", (data) => {
     console.log(data);
   });
 
   // handle the event sent with socket.emit()
-  socket.on('salutations', (elem1, elem2, elem3) => {
+  socket.on("salutations", (elem1, elem2, elem3) => {
     console.log(elem1, elem2, elem3);
   });
+});
+
+
+http.listen(port, function () {
+  console.log('listening on *:' + port);
 });
